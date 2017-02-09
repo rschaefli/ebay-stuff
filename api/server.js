fd523fedc336700246e92512c41dabd035416784
Ricardo Schaefli <ricardo.schaefli@gmail.com>
2017-02-08 20:08:21 -0800

    add index html page, add sandbox config

--- api/server.js
+++ api/server.js
@@ -1,29 +1,31 @@
 'use strict';
 
-var express = require('express'),
+var config = require('./config/sandbox.js'),
+    express = require('express'),
     app = express(),
     ebayApi = require('./lib/ebay-api');
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+  console.log(config.runame);
+  res.send('Hello World!');
 });
 
 app.get('/ebay/listings', function(req, res) {
-    ebayApi.getListings({
-        currentPage:  1,
-        maxPages: 100,
-        //operationName: "findItemsByKeywords",
-        //responseFieldName: "findItemsByKeywordsResponse",
-        operationName: "findCompletedItems",
-        responseFieldName: "findCompletedItemsResponse",
-        query: "pokemon booster"
-    }).then(function(listings) {
-        res.json({
-            listings: listings
-        });
+  ebayApi.getListings({
+    currentPage:  1,
+    maxPages: 100,
+    //operationName: "findItemsByKeywords",
+    //responseFieldName: "findItemsByKeywordsResponse",
+    operationName: "findCompletedItems",
+    responseFieldName: "findCompletedItemsResponse",
+    query: "pokemon booster"
+  }).then(function(listings) {
+    res.json({
+      listings: listings
     });
+  });
 });
 
 app.listen(3000, function () {
-    console.log('API server running on port 3000!');
+  console.log('API server running on port 3000!');
 });
--- app/server.js
+++ app/server.js
@@ -2,19 +2,28 @@
 
 var express = require('express'),
     app = express(),
-    rp = require('request-promise');
+    rp = require('request-promise'),
+    path = require('path');
 
-app.get('/', function (req, res) {
-    rp('http://localhost:3000/ebay/listings')
-        .then(function(result) {
-            res.json(result);
-        })
-        .catch(function(err) {
-            console.error(err);
-            res.json(err);
-        });
+app.get('/', function(req, res) {
+  return rp('http://localhost:3000/')
+    .then(function(result) {
+      return res.json(result);
+    });
+  //return res.sendFile(path.join(__dirname+'/public/html/index.html'));
+});
+
+app.get('/listings', function (req, res) {
+  return rp('http://localhost:3000/ebay/listings')
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.json(err);
+    });
 });
 
 app.listen(5000, function () {
-    console.log('Web server running on port 5000!');
+  console.log('Web server running on port 5000!');
 });
